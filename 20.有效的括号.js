c994ab17a627a4c62b225d3197d0915863e85411
zchq88 <zchq88@aliyun.com>
2019-02-28 10:18:22 +0800

    [20] 有效的括号

---
+++ 20.有效的括号.js
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (36.41%)
+ * Total Accepted:    46.9K
+ * Total Submissions: 128.7K
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *
+ *
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ *
+ *
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true
+ *
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = function (s) {
+  const dict = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+  const stack = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const char of s) {
+    if (dict[char]) {
+      stack.push(dict[char]);
+    } else if (stack.pop() !== char) {
+      return false;
+    }
+  }
+  if (stack.length !== 0) return false;
+  return true;
+};
+
+/*
+console.log(isValid('()'));
+console.log(isValid('()[]{}'));
+console.log(isValid('(]'));
+console.log(isValid('([)]'));
+console.log(isValid('{[]}'));
+// */
