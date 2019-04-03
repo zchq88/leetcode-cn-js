b41ad8af419efbb572f118669cdd9c7e24f37bf1
zchq88 <zchq88@aliyun.com>
2019-04-03 16:08:01 +0800

    [102] 二叉树的层次遍历

---
+++ 102.二叉树的层次遍历.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=javascript
+ *
+ * [102] 二叉树的层次遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (53.39%)
+ * Total Accepted:    21K
+ * Total Submissions: 38.3K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ *
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ *
+ * 返回其层次遍历结果：
+ *
+ * [
+ * ⁠ [3],
+ * ⁠ [9,20],
+ * ⁠ [15,7]
+ * ]
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+const levelOrder = function (root) {
+  // 递归深度优先解决方法
+  const res = [];
+  function dfs(node, level) {
+    if (!res[level]) res[level] = [];
+    res[level].push(node.val);
+    if (node.left) dfs(node.left, level + 1);
+    if (node.right) dfs(node.right, level + 1);
+  }
+  if (root === null) {
+    return res;
+  }
+  dfs(root, 0);
+  return res;
+};
